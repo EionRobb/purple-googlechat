751a0617f4f8cab3eaf3ad2591f683d590de9200
Eion Robb <eion@robbmob.com>
2016-01-10 23:22:50 +1300

    Add pblite_encode() function

--- libhangouts.c
+++ libhangouts.c
@@ -9,6 +9,9 @@
 #include "debug.h"
 #include "plugin.h"
 #include "version.h"
+#ifdef _WIN32
+#include "win32/win32dep.h"
+#endif
 
 #include "hangouts.pb-c.h"
 
@@ -86,6 +89,7 @@ static ProtobufCAllocator glib_protobufc_allocator = {
 };
 
 gboolean pblite_decode(ProtobufCMessage *message, JsonArray *pblite_array, gboolean ignore_first_item);
+JsonArray *pblite_encode(ProtobufCMessage *message);
 
 
 #define STRUCT_MEMBER_P(struct_p, struct_offset) \
@@ -187,6 +191,87 @@ pblite_decode_field(const ProtobufCFieldDescriptor *field, JsonNode *value, gpoi
 	}
 }
 
+
+JsonNode *
+pblite_encode_field(const ProtobufCFieldDescriptor *field, gpointer value)
+{
+	JsonNode *node = NULL;
+	
+	switch (field->type) {
+		case PROTOBUF_C_TYPE_INT32:
+		case PROTOBUF_C_TYPE_UINT32:
+		case PROTOBUF_C_TYPE_SFIXED32:
+		case PROTOBUF_C_TYPE_FIXED32:
+		case PROTOBUF_C_TYPE_FLOAT:
+		case PROTOBUF_C_TYPE_ENUM: {
+			uint32_t * member = value;
+			node = json_node_new(JSON_NODE_VALUE);
+			json_node_set_int(node, *member);
+			break;
+		}
+			
+		case PROTOBUF_C_TYPE_SINT32: {
+			int32_t * member = value;
+			node = json_node_new(JSON_NODE_VALUE);
+			json_node_set_int(node, *member);
+			break;
+		}
+	
+		case PROTOBUF_C_TYPE_INT64:
+		case PROTOBUF_C_TYPE_UINT64:
+		case PROTOBUF_C_TYPE_SFIXED64:
+		case PROTOBUF_C_TYPE_FIXED64:
+		case PROTOBUF_C_TYPE_DOUBLE: {
+			uint64_t * member = value;
+			node = json_node_new(JSON_NODE_VALUE);
+			json_node_set_int(node, *member);
+			break;
+		}
+		
+		case PROTOBUF_C_TYPE_SINT64: {
+			int64_t * member = value;
+			node = json_node_new(JSON_NODE_VALUE);
+			json_node_set_int(node, *member);
+			break;
+		}
+		
+		case PROTOBUF_C_TYPE_BOOL: {
+			protobuf_c_boolean * member = value;
+			node = json_node_new(JSON_NODE_VALUE);
+			json_node_set_int(node, *member);
+			break;
+		}
+			
+		case PROTOBUF_C_TYPE_STRING: {
+			char **pstr = value;
+			node = json_node_new(JSON_NODE_VALUE);
+			json_node_set_string(node, *pstr);		
+			break;
+		}
+		
+		case PROTOBUF_C_TYPE_BYTES: {
+			ProtobufCBinaryData *bd = value;
+			gchar *b64_data;
+			
+			b64_data = g_base64_encode(bd->data, bd->len);
+			node = json_node_new(JSON_NODE_VALUE);
+			json_node_set_string(node, b64_data);
+			g_free(b64_data);
+			break;
+		}
+		
+		case PROTOBUF_C_TYPE_MESSAGE: {
+			ProtobufCMessage **pmessage = value;
+			
+			node = json_node_new(JSON_NODE_ARRAY);
+			json_node_take_array(node, pblite_encode(*pmessage));
+			break;
+		}
+	}
+	
+	return node;
+}
+
 gboolean
 pblite_decode(ProtobufCMessage *message, JsonArray *pblite_array, gboolean ignore_first_item)
 {
@@ -199,6 +284,11 @@ pblite_decode(ProtobufCMessage *message, JsonArray *pblite_array, gboolean ignor
 	len = json_array_get_length(pblite_array);
 #ifdef DEBUG
 	printf("pblite_decode of %s with length %d\n", descriptor->name, len);
+	
+	if (JSON_NODE_HOLDS_OBJECT(json_array_get_element(pblite_array, len - 1))) {
+		printf("ZOMG the last element is an object\n");
+		exit(-1);
+	}
 #endif
 	
 	for (i = offset; i < len; i ++) {
@@ -225,7 +315,7 @@ pblite_decode(ProtobufCMessage *message, JsonArray *pblite_array, gboolean ignor
 			JsonArray *value_array = json_node_get_array(value);
 			guint j, array_len;
 			size_t siz;
-			//size_t *n_ptr;
+			size_t *n_ptr;
 			
 			array_len = json_array_get_length(value_array);
 #ifdef DEBUG
@@ -234,7 +324,8 @@ pblite_decode(ProtobufCMessage *message, JsonArray *pblite_array, gboolean ignor
 			
 			//see protobuf-c.c:3068
 			siz = sizeof_elt_in_repeated_array(field->type);
-			//n_ptr = STRUCT_MEMBER_PTR(size_t, message, field.quantifier_offset);
+			n_ptr = STRUCT_MEMBER_PTR(size_t, message, field->quantifier_offset);
+			*n_ptr = array_len;
 			STRUCT_MEMBER(void *, message, field->offset) = g_malloc0(siz * array_len);
 			for (j = 0; j < array_len; j++) {
 				pblite_decode_field(field, json_array_get_element(value_array, j), STRUCT_MEMBER_P(message, field->offset + siz * j));
@@ -247,6 +338,47 @@ pblite_decode(ProtobufCMessage *message, JsonArray *pblite_array, gboolean ignor
 	//TODO repeate the array loop with an object loop, if the last element of the array is an object
 	
 	return TRUE;
+}
+
+JsonArray *
+pblite_encode(ProtobufCMessage *message)
+{
+	//Maiku asked for more dinosaurs
+	JsonArray *pblite = json_array_new();
+	const ProtobufCMessageDescriptor *descriptor = message->descriptor;
+	guint i;
+	
+	for (i = 0; i < descriptor->n_fields; i++) {
+		//const ProtobufCFieldDescriptor field_descriptor = descriptor->fields[i];
+		const ProtobufCFieldDescriptor *field_descriptor = descriptor->fields + i;
+		void *field = STRUCT_MEMBER_P(message, field_descriptor->offset);
+		JsonNode *encoded_value;
+		
+		if (field_descriptor->label == PROTOBUF_C_LABEL_REPEATED) {
+			guint j;
+			size_t siz;
+			size_t *n_ptr;
+			JsonArray *value_array;
+			
+			siz = sizeof_elt_in_repeated_array(field_descriptor->type);
+			n_ptr = STRUCT_MEMBER_PTR(size_t, message, field_descriptor->quantifier_offset);
+			
+			value_array = json_array_new();
+			for (j = 0; j < *n_ptr; j++) {
+				field = STRUCT_MEMBER_P(message, field_descriptor->offset + siz * j);
+				json_array_add_element(value_array, pblite_encode_field(field_descriptor, field));
+			}
+			encoded_value = json_node_new(JSON_NODE_ARRAY);
+			json_node_take_array(encoded_value, value_array);
+		} else {
+			encoded_value = pblite_encode_field(field_descriptor, field);
+		}
+		
+		//need to insert at point [field_descriptor->id - 1] of the array, somehow
+		json_array_add_element(pblite, encoded_value);
+	}
+	
+	return pblite;
 }
 
 static const gchar *test_string1 = "W1s4ODMsW3sicCI6IntcIjFcIjp7XCIxXCI6e1wiMVwiOntcIjFcIjoxLFwiMlwiOjF9fSxcIjRcIjpcIjE0NTIzMjgxNzMxNDZcIixcIjVcIjpcIlM4NDdcIn0sXCIyXCI6e1wiMVwiOntcIjFcIjpcImJhYmVsXCIsXCIyXCI6XCJjb25zZXJ2ZXIuZ29vZ2xlLmNvbVwifSxcIjJcIjpcIltcXFwiY2J1XFxcIixbW1syLG51bGwsXFxcIjIyOTY4MjM1MDAwMDI1OTEzNDBcXFwiLFtbMF1cXG5dXFxuLDE0NTIzMjgxNzI3ODcwMDAsWzFdXFxuXVxcbixudWxsLFtbW1xcXCJVZ3hHZHBDS19tU3JoQlg4aHJ4NEFhQUJBUVxcXCJdXFxuLFtcXFwiMTEwMTc0MDY2Mzc1MDYxMTE4NzI3XFxcIixcXFwiMTEwMTc0MDY2Mzc1MDYxMTE4NzI3XFxcIl1cXG4sMTQ1MjMyODE3Mjg5NjcyMyxbW1xcXCIxMTAxNzQwNjYzNzUwNjExMTg3MjdcXFwiLFxcXCIxMTAxNzQwNjYzNzUwNjExMTg3MjdcXFwiXVxcbixcXFwiMTA1MDQ3MTY0XFxcIiwzMF1cXG4sbnVsbCxudWxsLFtudWxsLG51bGwsW1tbMCxcXFwiYnV0IHRvIGxvYWQgZnJvbSBQRU0sIHlvdSBnbyB2aWEgREVSXFxcIl1cXG5dXFxuXVxcbl1cXG4sbnVsbCxudWxsLG51bGwsbnVsbCxcXFwiNy1IMFo3LVI5NVg4OG1TRzlrLUNkQVxcXCIsbnVsbCxudWxsLDEsMiwxLG51bGwsbnVsbCxbMV1cXG4sbnVsbCxudWxsLDEsMTQ1MjMyODE3Mjg5NjcyMyxudWxsLFtcXFwiNy1IMFo3LVI5NVg4OG1TRzlrLUNkQVxcXCIsMjMsbnVsbCwxNDUyMzI4MTcyODk2NzIzXVxcbixudWxsLDJdXFxuXVxcbixudWxsLG51bGwsbnVsbCxudWxsLG51bGwsbnVsbCxudWxsLG51bGwsbnVsbCxbW1xcXCJVZ3hHZHBDS19tU3JoQlg4aHJ4NEFhQUJBUVxcXCJdXFxuLDEsbnVsbCxbbnVsbCxudWxsLG51bGwsbnVsbCxudWxsLG51bGwsW1tcXFwiMTEwMTc0MDY2Mzc1MDYxMTE4NzI3XFxcIixcXFwiMTEwMTc0MDY2Mzc1MDYxMTE4NzI3XFxcIl1cXG4sMF1cXG4sMiwzMCxbMV1cXG4sW1xcXCIxMTE1MjMxNTA2MjAyNTAxNjU4NjZcXFwiLFxcXCIxMTE1MjMxNTA2MjAyNTAxNjU4NjZcXFwiXVxcbiwxMzY3NjQ1ODMxNTYyMDAwLDE0NTIzMjgxNzI4OTY3MjMsMTM2NzY0NTgzMTU2MjAwMCxudWxsLG51bGwsW1tbMV1cXG4sMV1cXG5dXFxuXVxcbixudWxsLG51bGwsbnVsbCxbW1tcXFwiMTExNTIzMTUwNjIwMjUwMTY1ODY2XFxcIixcXFwiMTExNTIzMTUwNjIwMjUwMTY1ODY2XFxcIl1cXG4sMF1cXG4sW1tcXFwiMTEwMTc0MDY2Mzc1MDYxMTE4NzI3XFxcIixcXFwiMTEwMTc0MDY2Mzc1MDYxMTE4NzI3XFxcIl1cXG4sMF1cXG5dXFxuLDAsMiwxLG51bGwsW1tcXFwiMTEwMTc0MDY2Mzc1MDYxMTE4NzI3XFxcIixcXFwiMTEwMTc0MDY2Mzc1MDYxMTE4NzI3XFxcIl1cXG4sW1xcXCIxMTE1MjMxNTA2MjAyNTAxNjU4NjZcXFwiLFxcXCIxMTE1MjMxNTA2MjAyNTAxNjU4NjZcXFwiXVxcbl1cXG4sW1tbXFxcIjExMTUyMzE1MDYyMDI1MDE2NTg2NlxcXCIsXFxcIjExMTUyMzE1MDYyMDI1MDE2NTg2NlxcXCJdXFxuLFxcXCJNaWtlIFJ1cHJlY2h0XFxcIiwyLG51bGwsMiwyXVxcbixbW1xcXCIxMTAxNzQwNjYzNzUwNjExMTg3MjdcXFwiLFxcXCIxMTAxNzQwNjYzNzUwNjExMTg3MjdcXFwiXVxcbixcXFwiRWlvbiBSb2JiXFxcIiwyLG51bGwsMiwyXVxcbl1cXG4sbnVsbCxudWxsLG51bGwsWzFdXFxuLDBdXFxuXVxcbixbWzIsbnVsbCxcXFwiMjI5NjgyMzUwMDAwMjU5MTM0MFxcXCIsW1swXVxcbl1cXG4sMTQ1MjMyODE3Mjc4NzAwMCxbMV1cXG5dXFxuLG51bGwsbnVsbCxudWxsLFtbXFxcIlVneEdkcENLX21TcmhCWDhocng0QWFBQkFRXFxcIl1cXG4sW1xcXCIxMTAxNzQwNjYzNzUwNjExMTg3MjdcXFwiLFxcXCIxMTAxNzQwNjYzNzUwNjExMTg3MjdcXFwiXVxcbiwxNDUyMzI4MTcyNzg3MDAwLDNdXFxuXVxcbixbWzIsbnVsbCxcXFwiMjI5NjgyMzUwMDAwMjU5MTM0MFxcXCIsW1swXVxcbl1cXG4sMTQ1MjMyODE3Mjc4NzAwMCxbMV1cXG5dXFxuLG51bGwsbnVsbCxudWxsLG51bGwsbnVsbCxudWxsLFtbXFxcIjExMDE3NDA2NjM3NTA2MTExODcyN1xcXCIsXFxcIjExMDE3NDA2NjM3NTA2MTExODcyN1xcXCJdXFxuLFtcXFwiVWd4R2RwQ0tfbVNyaEJYOGhyeDRBYUFCQVFcXFwiXVxcbiwxNDUyMzI4MTcyODk2NzIzXVxcbl1cXG5dXFxuXVxcblwifX0ifV1dDQpdDQo=";
@@ -320,6 +452,7 @@ json_decode(const gchar *data, gssize len)
 		purple_debug_error("hangouts", "Error parsing JSON: %s\n", data);
 	} else {
 		root = json_node_copy(json_parser_get_root(parser));
+#ifdef DEBUG
 		switch(json_node_get_node_type(root)) {
 			case JSON_NODE_OBJECT:
 				printf("object\n");
@@ -335,6 +468,7 @@ json_decode(const gchar *data, gssize len)
 				break;
 		}
 		//printf("hangouts - %d\n", json_node_get_value_type(root));
+#endif
 		//g_object_ref(root);
 	}
 	g_object_unref(parser);
@@ -342,70 +476,142 @@ json_decode(const gchar *data, gssize len)
 	return root;
 }
 
+static void
+hangouts_process_data_chunks(const gchar *data, gsize len)
+{
+	JsonArray *chunks;
+	guint i, num_chunks;
+	
+	chunks = json_node_get_array(json_decode(data, len));
+	
+	for (i = 0, num_chunks = json_array_get_length(chunks); i < num_chunks; i++) {
+		JsonArray *chunk;
+		JsonArray *array;
+		JsonNode *array0;
+		
+		chunk = json_array_get_array_element(chunks, i);
+		
+		array = json_array_get_array_element(chunk, 1);
+		array0 = json_array_get_element(array, 0);
+		if (JSON_NODE_HOLDS_VALUE(array0)) {
+			//probably a nooooop
+			if (g_strcmp0(json_node_get_string(array0), "noop") == 0) {
+				//A nope ninja delivers a wicked dragon kick
+#ifdef DEBUG
+				printf("noop\n");
+#endif
+			}
+		} else {
+			const gchar *p = json_object_get_string_member(json_node_get_object(array0), "p");
+			JsonObject *wrapper = json_node_get_object(json_decode(p, -1));
+			
+			if (json_object_has_member(wrapper, "3")) {
+				const gchar *new_client_id = json_object_get_string_member(json_object_get_object_member(wrapper, "3"), "2");
+				purple_debug_info("hangouts", "Received new client_id: %s\n", new_client_id);
+			}
+			if (json_object_has_member(wrapper, "2")) {
+				const gchar *wrapper22 = json_object_get_string_member(json_object_get_object_member(wrapper, "2"), "2");
+				JsonArray *pblite_message = json_node_get_array(json_decode(wrapper22, -1));
+				
+				if (g_strcmp0(json_array_get_string_element(pblite_message, 0), "cbu") == 0) {
+					BatchUpdate batch_update = BATCH_UPDATE__INIT;
+					
+					pblite_decode((ProtobufCMessage *) &batch_update, pblite_message, TRUE);
+				}
+				
+				json_array_unref(pblite_message);
+			}
+			
+			json_object_unref(wrapper);
+		}
+	}
+	
+	json_array_unref(chunks);
+}
+
+static int
+read_all(int fd, void *buf, size_t len)
+{
+	unsigned int rs = 0;
+	while(rs < len)
+	{
+		int rval = read(fd, buf + rs, len - rs);
+		if (rval == 0)
+			break;
+		if (rval < 0)
+			return rval;
+
+		rs += rval;
+	}
+	return rs;
+}
+
+static void
+hangouts_process_channel(int fd)
+{
+	gsize len, lenpos = 0;
+	gchar len_str[256];
+	gchar *chunk;
+	
+	while(read(fd, len_str + lenpos, 1) > 0) {
+		//read up until \n
+		if (len_str[lenpos] == '\n') {
+			//convert to int, use as length of string to read
+			len_str[lenpos] = '\0';
+#ifdef DEBUG
+			printf("len_str is %s\n", len_str);
+#endif
+			len = atoi(len_str);
+			
+			chunk = g_new(gchar, len * 2);
+			//XX - could be a utf-16 length*2 though, so read up until \n????
+			
+			if (read_all(fd, chunk, len) > 0) {
+				//throw chunk to hangouts_process_data_chunks
+				hangouts_process_data_chunks(chunk, len);
+			}
+			
+			g_free(chunk);
+			
+			lenpos = 0;
+		} else {
+			lenpos = lenpos + 1;
+		}
+	}
+}
 
 static void
 init_plugin(PurplePlugin *plugin)
 {
+}
+	
+
+PURPLE_INIT_PLUGIN(hangouts, init_plugin, info);
+
+#ifdef DEBUG
+int main (int argc, const char * argv[])
+{
+	g_type_init();
+	
 	(void) glib_protobufc_allocator;
 	(void) test_string1;
 	gsize len;
 	gchar *test_string1_real = (gchar*) g_base64_decode(test_string1, &len);
 	
-	JsonArray *chunks;
-	JsonArray *chunk;
-	JsonArray *array;
-	JsonNode *array0;
+	hangouts_process_data_chunks(test_string2, -1);
+	hangouts_process_data_chunks(test_string1_real, len);
 	
-	chunks = json_node_get_array(json_decode(test_string2, -1));
-	chunks = json_node_get_array(json_decode(test_string1_real, len));
+#ifndef O_BINARY 
+#	define O_BINARY 0 
+#endif
+#ifndef O_RDONLY 
+#	define O_RDONLY 0 
+#endif
+	printf("reading file test-response-1.txt");
+	int fd = open("test-response-1.txt", O_RDONLY | O_BINARY);
+	hangouts_process_channel(fd);
+	close(fd);
 	
-	
-	//TODO not be lazy
-	chunk = json_array_get_array_element(chunks, 0);
-	
-	array = json_array_get_array_element(chunk, 1);
-	array0 = json_array_get_element(array, 0);
-	if (JSON_NODE_HOLDS_VALUE(array0)) {
-		//probably a nooooop
-		if (g_strcmp0(json_node_get_string(array0), "noop") == 0) {
-			//A nope ninja delivers a wicked dragon kick
-		}
-	} else {
-		//TODO not be lazy
-		const gchar *p = json_object_get_string_member(json_node_get_object(array0), "p");
-		JsonObject *wrapper = json_node_get_object(json_decode(p, -1));
-		
-		if (json_object_has_member(wrapper, "3")) {
-			const gchar *new_client_id = json_object_get_string_member(json_object_get_object_member(wrapper, "3"), "2");
-			purple_debug_info("hangouts", "Received new client_id: %s\n", new_client_id);
-		}
-		if (json_object_has_member(wrapper, "2")) {
-			const gchar *wrapper22 = json_object_get_string_member(json_object_get_object_member(wrapper, "2"), "2");
-			JsonArray *pblite_message = json_node_get_array(json_decode(wrapper22, -1));
-			
-			if (g_strcmp0(json_array_get_string_element(pblite_message, 0), "cbu") == 0) {
-				BatchUpdate batch_update = BATCH_UPDATE__INIT;
-				
-				pblite_decode((ProtobufCMessage *) &batch_update, pblite_message, TRUE);
-			}
-			
-			json_array_unref(pblite_message);
-		}
-		
-		json_object_unref(wrapper);
-	}
-	
-	json_array_unref(chunks);
-	
-	exit(0);
-}
-
-PURPLE_INIT_PLUGIN(hangouts, init_plugin, info);
-
-int main (int argc, const char * argv[])
-{
-	g_type_init();
-	
-	init_plugin(NULL);
 	return 0;
 }
+#endif

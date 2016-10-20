91d91c069dc11ad66744f089cb8ab5c5c68fffcd
Eion Robb <eion@robbmob.com>
2016-10-20 20:27:43 +1300

    Point to @dequis useful YouTube video about how to auth

--- libhangouts.c
+++ libhangouts.c
@@ -343,10 +343,10 @@ hangouts_login(PurpleAccount *account)
 		hangouts_oauth_refresh_token(ha);
 	} else {
 		//TODO get this code automatically
-		purple_notify_uri(pc, HANGOUTS_API_OAUTH2_AUTHORIZATION_CODE_URL);
-		purple_request_input(pc, _("Authorization Code"), HANGOUTS_API_OAUTH2_AUTHORIZATION_CODE_URL,
-			_ ("Please paste the Google OAuth code here"),
-			NULL, FALSE, FALSE, NULL, 
+		purple_notify_uri(pc, "https://www.youtube.com/watch?v=hlDhp-eNLMU");
+		purple_request_input(pc, _("Authorization Code"), "https://www.youtube.com/watch?v=hlDhp-eNLMU",
+			_ ("Please follow the YouTube video to get the OAuth code"),
+			_ ("and then paste the Google OAuth code here"), FALSE, FALSE, NULL, 
 			_("OK"), G_CALLBACK(hangouts_authcode_input_cb), 
 			_("Cancel"), G_CALLBACK(hangouts_authcode_input_cancel_cb), 
 			purple_request_cpar_from_connection(pc), ha);

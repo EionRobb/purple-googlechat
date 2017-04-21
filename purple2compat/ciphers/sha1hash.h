8f236b7effbed905e62b998fc712d03abe8247f9
Eion Robb <eion@robbmob.com>
2017-04-21 18:29:48 +1200

    Make sure we don't double-define PurpleHash if we're using both sha1 and sha256

--- purple2compat/ciphers/sha1hash.h
+++ purple2compat/ciphers/sha1hash.h
@@ -3,11 +3,16 @@
 
 #include "cipher.h"
 
-#define PurpleHash		PurpleCipherContext
 #define purple_sha1_hash_new()	purple_cipher_context_new(purple_ciphers_find_cipher("sha1"), NULL)
-#define purple_hash_append	purple_cipher_context_append
-#define purple_hash_digest_to_str(ctx, data, size) \
+
+#ifndef PurpleHash
+#	define PurpleHash		PurpleCipherContext
+#	define purple_hash_append	purple_cipher_context_append
+#	define purple_hash_digest_to_str(ctx, data, size) \
 				purple_cipher_context_digest_to_str(ctx, size, data, NULL)
-#define purple_hash_destroy	purple_cipher_context_destroy
+#	define purple_hash_digest(ctx, data, size) \
+				purple_cipher_context_digest(ctx, size, data, NULL)
+#	define purple_hash_destroy	purple_cipher_context_destroy
+#endif /*PurpleHash*/
 
 #endif /*_CIPHERS_SHA1HASH_H_*/

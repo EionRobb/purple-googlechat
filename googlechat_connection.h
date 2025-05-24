6bccb9120b16bc8aeed991fb3b529d4bf2b306b4
Eion Robb <eion@robbmob.com>
2025-05-25 00:50:30 +1200

    Update protobuf file to try help issue #85
    Couldn't quite get to event type 83 :(

--- googlechat_connection.h
+++ googlechat_connection.h
@@ -103,5 +103,6 @@ GOOGLECHAT_DEFINE_API_REQUEST_FUNC(set_custom_status, SetCustomStatus, "set_cust
 GOOGLECHAT_DEFINE_API_REQUEST_FUNC(update_group, UpdateGroup, "update_group");
 GOOGLECHAT_DEFINE_API_REQUEST_FUNC(block_entity, BlockEntity, "block_entity");
 GOOGLECHAT_DEFINE_API_REQUEST_FUNC(list_members, ListMembers, "list_members");
+GOOGLECHAT_DEFINE_API_REQUEST_FUNC(update_reaction, UpdateReaction, "update_reaction");
 
 #endif /*_GOOGLECHAT_CONNECTION_H_*/

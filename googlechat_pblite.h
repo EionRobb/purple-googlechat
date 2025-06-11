e9082837b5d4aa3bf54fd15f3154fb0703372a7e
Eion Robb <eion@robbmob.com>
2025-06-11 21:40:19 +1200

    Super important critical urgent priority copyright year bump

--- googlechat_pblite.h
+++ googlechat_pblite.h
@@ -1,6 +1,6 @@
 /*
  * GoogleChat Plugin for libpurple/Pidgin
- * Copyright (c) 2015-2016 Eion Robb, Mike Ruprecht
+ * Copyright (c) 2015-2025 Eion Robb, Mike Ruprecht
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
